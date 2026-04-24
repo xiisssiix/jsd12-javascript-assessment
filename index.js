596e87e6012d806aa5e73f950770869f0db383fb
unknown <xiisssiix@gmail.com>
2026-04-24 13:48:52 +0700

    set-conditions

--- index.js
+++ index.js
@@ -13,4 +13,55 @@ class Field {
     this.locationY = 0; 
     this.field[0][0] = pathCharacter; 
   }
+
+  print() {
+    const displayString = this.field.map(row => row.join('')).join('\n');
+    console.log(displayString);
+  }
+  runGame() {
+    let playing = true;
+    while (playing) {
+      this.print(); 
+      this.askQuestion(); 
+      
+      if (!this.isInBounds()) { 
+        console.log("เดินออกนอกเกม คุณแพ้แล้ว");
+        playing = false;
+        break;
+      } else if (this.isHole()) { 
+        console.log("ตกหลุม Game Over ครับ");
+        playing = false;
+        break;
+      } else if (this.isHat()) { 
+        console.log("ยินดีด้วยครับ คุณเจอหมวกแล้ว!");
+        playing = false;
+        break;
+      }
+      
+      this.field[this.locationY][this.locationX] = pathCharacter;
+    }
+  }
+
+  askQuestion() {
+    const answer = prompt('Where to? (W, S, A, D): ').toUpperCase();
+    switch (answer) {
+      case 'W': this.locationY -= 1; break; 
+      case 'S': this.locationY += 1; break; 
+      case 'A': this.locationX -= 1; break; 
+      case 'D': this.locationX += 1; break; 
+      default:
+        console.log("Enter W, S, A, or D");
+        this.askQuestion();
+        break;
+    }
+  }
+
+  isInBounds() {
+    return (
+      this.locationY >= 0 &&
+      this.locationX >= 0 &&
+      this.locationY < this.field.length &&
+      this.locationX < this.field[0].length
+    );
+  }
 }
